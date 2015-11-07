d9ea6550a26b24a6c822d2ac5eaac963398f6f32
James Hall <jahall@ragecastle.org>
2015-11-07 16:56:00 -0500

    updated things

--- app/src/main/java/org/ragecastle/panicbutton/MainActivity.java
+++ app/src/main/java/org/ragecastle/panicbutton/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,13 +18,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View v){
         int id = v.getId();
+        String phoneNum = "5555555555";
 
         switch(id){
             case R.id.panic_button:
                 try{
 
                     SmsManager smsManager = SmsManager.getDefault();
-                    smsManager.sendTextMessage("6315149716", null, "Send", null, null);
+                    smsManager.sendTextMessage(phoneNum, null, "Send", null, null);
 
                 } catch (Exception e){
                     Log.d(LOG_TAG, "Shits broke");
